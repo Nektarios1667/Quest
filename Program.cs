8e928522799358c54516b34f9a1ec4daf2d516ab
Nektarios1667 <nektarioslavdas@gmail.com>
2025-06-29 20:35:02 -0400

    Launch profiles for editor and game

--- Program.cs
+++ Program.cs
@@ -1,21 +1,35 @@
 ﻿global using System;
 global using System.Collections.Generic;
+
 global using Microsoft.Xna.Framework;
 global using Microsoft.Xna.Framework.Graphics;
 global using Microsoft.Xna.Framework.Input;
 global using MonoGame.Extended;
+
 global using Quest.Entities;
 global using Quest.Items;
 global using Quest.Managers;
 global using Quest.Tiles;
 global using Quest.Utilities;
 global using Quest.Decals;
+
 global using static Quest.Managers.TextureManager;
 
-#if EDITOR
-    using var game = new Quest.Editor.Window();
-    game.Run();
-#else
-using var game = new Quest.Window();
-game.Run();
-#endif
+using System.Linq;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Contains("--editor"))
+        {
+            using var editorApp = new Quest.Editor.Window();
+            editorApp.Run();
+        }
+        else
+        {
+            using var gameApp = new Quest.Window();
+            gameApp.Run();
+        }
+    }
+}
