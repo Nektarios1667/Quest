efcd1d61ac5aa7cfc228a6adf541550aed480531
Nektarios1667 <nektarioslavdas@gmail.com>
2025-11-22 16:27:14 -0500

    Removed test DELETEME

--- Managers/LevelManager.cs
+++ Managers/LevelManager.cs
@@ -434,7 +434,6 @@ public class LevelManager
             TileTypeID.Ice => new Ice(location),
             TileTypeID.SnowyGrass => new SnowyGrass(location),
             TileTypeID.Lamp => new Lamp(location),
-            TileType.DELETEME => new DELETEME(location),
             // TILEFROMID INSERT
             _ => throw new ArgumentException($"Unknown TileFromId TileType '{id}'.")
         };
