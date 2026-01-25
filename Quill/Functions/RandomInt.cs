3ae709da6fcf1ef65842b3de294a4ec49e291550
Nektarios1667 <nektarioslavdas@gmail.com>
2026-01-25 13:08:47 -0500

    Cleaned up Quill interpreter code

---
+++ Quill/Functions/RandomInt.cs
@@ -0,0 +1,17 @@
+﻿namespace Quest.Quill.Functions;
+public class RandomInt : IBuiltinFunction
+{
+    public FunctionResponse Run(string[] args)
+    {
+        if (args.Length != 2)
+            return new(false, "ParameterMismatch", $"Expected 2 parameters, got {args.Length}");
+        if (!int.TryParse(args[0], out int min))
+            return new(false, "TypeMismatch", $"Expected integer for first parameter, got '{args[0]}'");
+        if (!int.TryParse(args[1], out int max))
+            return new(false, "TypeMismatch", $"Expected integer for second parameter, got '{args[1]}'");
+        if (min > max)
+            return new(false, "ValueError", $"Minimum value {min} cannot be greater than maximum value {max}");
+        int randNum = new Random().Next(min, max + 1);
+        return new(true, null, null, new() { { "[return]", $"{randNum}" } });
+    }
+}
--- Quill/Interpreter.cs
+++ Quill/Interpreter.cs
@@ -1,11 +1,36 @@
 ﻿using NCalc;
 using Quest.Quill.Functions;
+using SharpDX.MediaFoundation.DirectX;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Quest.Quill;
 public static class Interpreter
 {
+    private static int l = 0;
+    private static Dictionary<string, string> Variables = [];
+    private static Dictionary<string, string> Parameters = [];
+    private static Dictionary<string, (int line, string[] parameters)> Functions = [];
+    private static List<int> Callbacks = [];
+    private static string[] Lines = [];
+
+    private readonly static Dictionary<string, IBuiltinFunction> BuiltinFunctions = new() {
+        { "readfile", new ReadFile() },
+        { "execute", new Execute() },
+        { "log", new Log() },
+        { "warn", new Warn() },
+        { "error", new Error() },
+        { "teleport", new Teleport() },
+        { "loadlevel", new LoadLevel() },
+        { "unloadlevel", new UnloadLevel() },
+        { "readlevel", new ReadLevel() },
+        { "give", new Give() },
+        { "getitem", new GetItem() },
+        { "getitem2d", new GetItem2D() },
+        { "contains", new Contains() },
+        { "randomint", new RandomInt() },
+    };
+
     private static readonly Dictionary<string, string> ExternalSymbols = new() {
         { "<ready>", "false" },
     };
@@ -60,21 +85,7 @@ public static class Interpreter
         ExternalSymbols["<fpslimit>"] = Constants.FPS.ToString();
         DebugManager.EndBenchmark("QuillSymbolsUpdate");
     }
-    private readonly static Dictionary<string, IBuiltinFunction> BuiltinFunctions = new() {
-        { "readfile", new ReadFile() },
-        { "execute", new Execute() },
-        { "log", new Log() },
-        { "warn", new Warn() },
-        { "error", new Error() },
-        { "teleport", new Teleport() },
-        { "loadlevel", new LoadLevel() },
-        { "unloadlevel", new UnloadLevel() },
-        { "readlevel", new ReadLevel() },
-        { "give", new Give() },
-        { "getitem", new GetItem() },
-        { "getitem2d", new GetItem2D() },
-        { "contains", new Contains() },
-    };
+    // Helpers
     static void FillParameters(ref Expression expr, Dictionary<string, string> vars)
     {
         foreach (var kvp in vars)
@@ -107,6 +118,7 @@ public static class Interpreter
             if (str.Contains(c)) return true;
         return false;
     }
+    // Run
     public static async Task RunScriptAsync(string code)
     {
         try
@@ -120,23 +132,23 @@ public static class Interpreter
     }
     private static async Task RunScriptCore(string code)
     {
-        Dictionary<string, string> variables = [];
-        Dictionary<string, string> parameters = [];
-        Dictionary<string, (int line, string[] parameters)> functions = [];
-        List<int> callbacks = [];
+        Variables = [];
+        Parameters = [];
+        Functions = [];
+        Callbacks = [];
 
-        string[] lines = code.Split("\n");
-        for (int l = 0; l < lines.Length; l++)
+        Lines = code.Split("\n");
+        for (l = 0; l < Lines.Length; l++)
         {
-            if (l < 0 || l >= lines.Length) break;
-            string line = lines[l].Trim();
+            if (l < 0 || l >= Lines.Length) break;
+            string line = Lines[l].Trim();
 
             // Comment
             if (line.StartsWith("//") || string.IsNullOrWhiteSpace(line)) continue;
 
             // Fill variables
-            ReplaceVariables(ref line, variables);
-            ReplaceVariables(ref line, parameters);
+            ReplaceVariables(ref line, Variables);
+            ReplaceVariables(ref line, Parameters);
             ReplaceVariables(ref line, ExternalSymbols);
 
             // Evaluations
@@ -150,194 +162,214 @@ public static class Interpreter
             });
 
             string[] parts = line.Split(' ');
+            string command = parts[0].Trim().ToLower();
 
-            // Variable declaration
-            if (parts[0] == "num")
+            switch (command)
             {
-                string varName = parts[1];
-                if (ContainsAny(varName, """`~!@#$%^&*()-=+[]{}\|;:'",<.>/?"""))
-                {
-                    Console.WriteLine($"Invalid variable name: {varName}");
-                    continue;
-                }
-                string varValue = parts[2];
-                Expression expr = new(varValue);
-                FillParameters(ref expr, variables);
-                var result = expr.Evaluate();
-                variables[varName] = result?.ToString() ?? "";
-                continue;
-            }
-            else if (parts[0] == "str")
-            {
-                string varName = parts[1];
-                if (ContainsAny(varName, """`~!@#$%^&*()-=+[]{}\|;:'",<.>/?"""))
-                {
-                    Console.WriteLine($"Invalid variable name: {varName}");
-                    continue;
-                }
-                string varValue = parts[2];
-                variables[varName] = varValue;
-                continue;
-            }
-            else if (parts[0] == "breakwhile")
-            {
-                if (parts.Length < 2)
-                    l = FindLine(lines, $"endwhile", l);
-                else
-                    l = FindLine(lines, $"endwhile {parts[1]}", l);
-            }
-            else if (parts[0] == "continuewhile")
-            {
-                if (parts.Length < 2)
-                    l = FindLineBackwards(lines, $"while", l) - 1; // -1 because of the l++ at the end of the loop
-                else
-                    l = FindLineBackwards(lines, $"while {parts[1]}", l) - 1; // -1 because of the l++ at the end of the loop
-            }
-            else if (parts[0] == "if")
-            {
-                string name = parts[1];
-                int conditionStart = 1;
-                if (name.StartsWith('.')) conditionStart = 2;
-                Expression expr = new(string.Join(' ', parts[conditionStart..]));
-                var result = expr.Evaluate();
-                if (result is bool b && !b)
-                    l = FindLine(lines, $"endif {(name.StartsWith('.') ? name : "")}", l);
-            }
-            else if (parts[0] == "endif") { } // Marker
-            else if (parts[0] == "while")
-            {
-                string name = parts[1];
-                int conditionStart = 1;
-                if (name.StartsWith('.')) conditionStart = 2;
-                Expression expr = new(string.Join(' ', parts[conditionStart..]));
-                FillParameters(ref expr, variables);
-                var result = expr.Evaluate();
-                if (result is bool b && !b)
-                    l = FindLine(lines, $"endwhile {(name.StartsWith('.') ? name : "")}", l);
-            }
-            else if (parts[0] == "endwhile")
-            {
-                if (parts.Length < 2)
-                    l = FindLineBackwards(lines, $"while", l) - 1; // -1 because of the l++ at the end of the loop
-                else
-                    l = FindLineBackwards(lines, $"while {parts[1]}", l) - 1; // -1 because of the l++ at the end of the loop
-            }
-            else if (parts[0] == "func")
-            {
-                string funcName = parts[1];
-                string[] funcParams = parts.Length <= 2 ? [] : parts[2..];
-                functions[funcName] = (l, funcParams);
-                l = FindLine(lines, $"endfunc {funcName}", l);
-                if (l == -1)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"Function '{funcName}' missing endfunc");
-                    Console.ResetColor();
+                case "num": HandleNum(parts); break;
+                case "str": HandleStr(parts); break;
+                case "breakwhile": HandleBreakWhile(parts); break;
+                case "continuewhile": HandleContinueWhile(parts); break;
+                case "if": HandleIf(parts); break;
+                case "endif": break; // Marker
+                case "while": HandleWhile(parts); break;
+                case "endwhile": HandleEndWhile(parts); break;
+                case "func": HandleFunc(parts); break;
+                case "endfunc": HandleEndFunc(); break;
+                case "call": HandleCall(parts); break;
+                case "sleep": await HandleSleep(parts); break;
+                case "wait": await HandleWait(parts); break;
+                default:
+                    if (BuiltinFunctions.TryGetValue(command, out var func))
+                        HandleBuiltin(func, parts);
+                    else
+                        Console.WriteLine($"Unknown command: {line}");
                     break;
-                }
-            } // Skip function definition
-            else if (parts[0] == "endfunc")
-            {
-                parameters.Clear();
-                if (callbacks.Count > 0)
-                {
-                    l = callbacks[^1];
-                    callbacks.RemoveAt(callbacks.Count - 1);
-                }
-            }
-            // User functions
-            else if (parts[0] == "call")
-            {
-                string funcName = parts[1];
-                var function = functions.TryGetValue(funcName, out var f) ? f : (-1, []);
-                if (function.line == -1)
-                {
-                    Console.WriteLine($"Function not found: {funcName}");
-                    continue;
-                }
-                // Get parameters
-                string[] stringParams = parts.Length <= 2 ? [] : string.Join(' ', parts[1..]).Split(',');
-                foreach (string param in stringParams)
-                {
-                    string[] kvp = param.Split(':');
-                    if (kvp.Length != 2)
-                    {
-                        Console.WriteLine($"Invalid parameter: {param}");
-                        continue;
-                    }
-                    string pName = kvp[0].Trim();
-                    string pValue = kvp[1].Trim();
-                    Expression expr = new(pValue);
-                    FillParameters(ref expr, variables);
-                    var result = expr.Evaluate();
-                    parameters[pName] = result?.ToString() ?? "";
-                }
-
-                // Check params
-                bool hasAllParams = true;
-                foreach (string p in function.parameters)
-                {
-                    if (!parameters.ContainsKey(p))
-                    {
-                        Console.WriteLine($"Function call '{funcName}' missing parameter '{p}' @ l{l}");
-                        hasAllParams = false;
-                        break;
-                    }
-                }
-                if (!hasAllParams) continue;
-
-                callbacks.Add(l);
-                l = function.line;
-            }
-            // Sleep
-            else if (parts[0] == "sleep")
-            {
-                string waitTimeStr = parts[1];
-                Expression expr = new(waitTimeStr);
-                FillParameters(ref expr, variables);
-                var result = expr.Evaluate();
-                if (result is int ms)
-                    await Task.Delay(ms);
-                else
-                    Console.WriteLine($"Invalid sleep time: {waitTimeStr}");
-            }
-            // Wait until
-            else if (parts[0] == "wait")
-            {
-                string waitTimeStr = parts[1];
-                string conditionStr = parts[2].Trim();
-                Expression expr = new(conditionStr);
-                FillParameters(ref expr, variables);
-                var result = expr.Evaluate();
-                if (result is not bool b || !int.TryParse(waitTimeStr, out int waitTime))
-                {
-                    Console.WriteLine($"Invalid wait command: {line}");
-                    continue;
-                }
-                if (!b)
-                {
-                    l--;
-                    await Task.Delay(waitTime);
-                }
-            }
-            // Builtin functions
-            else if (BuiltinFunctions.TryGetValue(parts[0], out var func))
-            {
-                // Get parameters
-                string[] externalParams = parts.Length <= 1 ? [] : string.Join(' ', parts[1..]).Split(',', StringSplitOptions.TrimEntries);
-
-                // Run
-                var resp = func.Run(externalParams);
-                if (!resp.Success)
-                    Console.WriteLine($"Builtin function '{parts[0]}' failed: {resp.ErrorType} - {resp.ErrorMessage}");
-                else
-                    foreach (var kvp in resp.OutputVariables)
-                        variables[kvp.Key] = kvp.Value;
-            }
-            else
-            {
-                Console.WriteLine($"Unknown command: {line}");
             }
         }
+    }
+    // Handlers
+    private static void HandleNum(string[] parts)
+    {
+        string varName = parts[1];
+        if (ContainsAny(varName, "`~!@#$%^&*()-=+[]{}\\|;:'\",<.>/?"))
+        {
+            Console.WriteLine($"Invalid variable name: {varName}");
+            return;
+        }
+
+        string varValue = parts[2];
+        Expression expr = new(varValue);
+        FillParameters(ref expr, Variables);
+        var result = expr.Evaluate();
+        Variables[varName] = result?.ToString() ?? "";
+    }
+
+    private static void HandleStr(string[] parts)
+    {
+        string varName = parts[1];
+        if (ContainsAny(varName, "`~!@#$%^&*()-=+[]{}\\|;:'\",<.>/?"))
+        {
+            Console.WriteLine($"Invalid variable name: {varName}");
+            return;
+        }
+
+        string varValue = parts[2];
+        Variables[varName] = varValue;
+    }
+
+    private static void HandleBreakWhile(string[] parts)
+    {
+        if (parts.Length < 2)
+            l = FindLine(Lines, "endwhile", l);
+        else
+            l = FindLine(Lines, $"endwhile {parts[1]}", l);
+    }
+
+    private static void HandleContinueWhile(string[] parts)
+    {
+        if (parts.Length < 2)
+            l = FindLineBackwards(Lines, "while", l) - 1;
+        else
+            l = FindLineBackwards(Lines, $"while {parts[1]}", l) - 1;
+    }
+
+    private static void HandleIf(string[] parts)
+    {
+        string name = parts[1];
+        int conditionStart = name.StartsWith('.') ? 2 : 1;
+        Expression expr = new(string.Join(' ', parts[conditionStart..]));
+        var result = expr.Evaluate();
+        if (result is bool b && !b)
+            l = FindLine(Lines, $"endif{(name.StartsWith('.') ? $" {name}" : "")}", l);
+    }
+
+    private static void HandleWhile(string[] parts)
+    {
+        string name = parts[1];
+        int conditionStart = name.StartsWith('.') ? 2 : 1;
+        Expression expr = new(string.Join(' ', parts[conditionStart..]));
+        FillParameters(ref expr, Variables);
+        var result = expr.Evaluate();
+        if (result is bool b && !b)
+            l = FindLine(Lines, $"endwhile{(name.StartsWith('.') ? $" {name}" : "")}", l);
+    }
+
+    private static void HandleEndWhile(string[] parts)
+    {
+        if (parts.Length < 2)
+            l = FindLineBackwards(Lines, "while", l) - 1;
+        else
+            l = FindLineBackwards(Lines, $"while {parts[1]}", l) - 1;
+    }
+
+    private static void HandleFunc(string[] parts)
+    {
+        string funcName = parts[1];
+        string[] funcParams = parts.Length <= 2 ? [] : parts[2..];
+        Functions[funcName] = (l, funcParams);
+        l = FindLine(Lines, $"endfunc {funcName}", l);
+        if (l == -1)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Function '{funcName}' missing endfunc");
+            Console.ResetColor();
+            throw new InvalidOperationException();
+        }
+    }
+
+    private static void HandleEndFunc()
+    {
+        Parameters.Clear();
+        if (Callbacks.Count > 0)
+        {
+            l = Callbacks[^1];
+            Callbacks.RemoveAt(Callbacks.Count - 1);
+        }
+    }
+
+    private static void HandleCall(string[] parts)
+    {
+        string funcName = parts[1];
+        var function = Functions.TryGetValue(funcName, out var f) ? f : (-1, []);
+        if (function.line == -1)
+        {
+            Console.WriteLine($"Function not found: {funcName}");
+            return;
+        }
+
+        string[] stringParams = parts.Length <= 2 ? [] : string.Join(' ', parts[1..]).Split(',');
+        foreach (string param in stringParams)
+        {
+            string[] kvp = param.Split(':');
+            if (kvp.Length != 2)
+            {
+                Console.WriteLine($"Invalid parameter: {param}");
+                continue;
+            }
+
+            string pName = kvp[0].Trim();
+            string pValue = kvp[1].Trim();
+            Expression expr = new(pValue);
+            FillParameters(ref expr, Variables);
+            Parameters[pName] = expr.Evaluate()?.ToString() ?? "";
+        }
+
+        foreach (string p in function.parameters)
+        {
+            if (!Parameters.ContainsKey(p))
+            {
+                Console.WriteLine($"Function call '{funcName}' missing parameter '{p}' @ l{l}");
+                return;
+            }
+        }
+
+        Callbacks.Add(l);
+        l = function.line;
+    }
+
+    private static async Task HandleSleep(string[] parts)
+    {
+        string waitTimeStr = parts[1];
+        Expression expr = new(waitTimeStr);
+        FillParameters(ref expr, Variables);
+        var result = expr.Evaluate();
+        if (result is int ms)
+            await Task.Delay(ms);
+        else
+            Console.WriteLine($"Invalid sleep time: {waitTimeStr}");
+    }
+
+    private static async Task HandleWait(string[] parts)
+    {
+        string waitTimeStr = parts[1];
+        string conditionStr = parts[2].Trim();
+        Expression expr = new(conditionStr);
+        FillParameters(ref expr, Variables);
+        var result = expr.Evaluate();
+        if (result is not bool b || !int.TryParse(waitTimeStr, out int waitTime))
+        {
+            Console.WriteLine($"Invalid wait command: {string.Join(' ', parts)}");
+            return;
+        }
+
+        if (!b)
+        {
+            l--;
+            await Task.Delay(waitTime);
+        }
+    }
+
+    private static void HandleBuiltin(IBuiltinFunction func, string[] parts)
+    {
+        string[] externalParams = parts.Length <= 1 ? [] : string.Join(' ', parts[1..])
+            .Split(',', StringSplitOptions.TrimEntries);
+
+        var resp = func.Run(externalParams);
+        if (!resp.Success)
+            Console.WriteLine($"Builtin function '{parts[0]}' failed: {resp.ErrorType} - {resp.ErrorMessage}");
+        else
+            foreach (var kvp in resp.OutputVariables)
+                Variables[kvp.Key] = kvp.Value;
     }
 }
